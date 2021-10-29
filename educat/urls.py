9c49e743c56600e9ab5151359522f127b2ec7c77
abdelkhalek jennani <jennaniabdel@gmail.com>
2021-10-29 06:37:05 +0100

    first commit

--- educat/urls.py
+++ educat/urls.py
@@ -1,6 +1,15 @@
 from django.urls import path
-from . import views 
+from . import views
 
 urlpatterns = [
-    path('', views.home),
+
+	path('', views.index),
+
+	path('categories/', views.category, name="categories"),
+
+	path('search/<str:query>/', views.search, name="search"),
+
+    path('module/<int:id>/', views.moduledetails, name="moduledetails"),
+
+
 ]
